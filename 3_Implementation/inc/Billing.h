fec22ed0352980477ed841598c06bcc810688916
UppalaNandana <101818853+UppalaNandana@users.noreply.github.com>
2022-03-31 22:38:36 +0530

    Update Billing.h

--- 3_Implementation/inc/Billing.h
+++ 3_Implementation/inc/Billing.h
@@ -5,22 +5,5 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
-/*
-struct items
-{
-    char item[20];
-    float price;
-    int quantity;
-};
-struct orders
-{
-    char customer[100];
 
-    char date[50];
-    int numOfItems;
-    struct items itm[20];
-};
-//struct items itm;
-struct orders ord;
-*/
 #endif
